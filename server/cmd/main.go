dc2172b172e0b2e552dc5922dc0adb43860a838e
a-naimankhan <naimakhanaibar@gmai.com>
2026-03-30 05:08:31 +0500

    feat : init project structure , domain and in-memory repositories

---
+++ server/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World") //just wrote so I could start normally and had in my mind that there was a first hello world!:D
+	//however no one would read it ahh
+	//but it is fine actually
+
+}
