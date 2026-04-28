94442d454b75a0fcb0cd1def6fa332cf9c4cb6de
a-naimankhan <naimakhanaibar@gmail.com>
2026-04-28 13:36:04 +0500

    feat : add gracefull shutdown with signal handling

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,6 +9,10 @@ import (
 	"File-management-system/server/internal/worker"
 	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -33,11 +37,47 @@ func startServer(cfg *config.Config) {
 
 	userSvc := service.NewUserService(userRepo, cfg.JWTSECRET)
 	workerPool := worker.NewPool(5) //IN future increase number of workers as tasks get higher
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	workerPool.Start(context.Background())
+
 	fileSvc := service.NewFileService(fileRepo, userRepo, cfg.StoragePath, workerPool)
 
 	h := delivery.NewHandler(userSvc, fileSvc, cfg.JWTSECRET)
 
 	router := h.InitRouter()
-	router.Run(":" + cfg.Port)
+
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
+	}
+
+	//running server in goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	log.Println("Listening on " + cfg.Port)
+
+	//waiting server to turn off
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	//5 sec to end the opertaions
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Server Shutdown: %s", err)
+	}
+
+	log.Println("Server exiting")
+
 }
